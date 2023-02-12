9eb12594b98b606266c6ba46031ac7946d21bfcc
ngdream <nguewofoss@gmail.com>
2023-02-12 03:08:45 +0100

    gg

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import logo from "../../assets/sLogo.png";
+
+const Footer = () => {
+    let date = new Date();
+    let year = date.getFullYear();
+    return (
+      <Container fluid className="footer">
+        <Row>
+          <Col md="4" className="footer-copywright">
+            <h3>Dedicated to creating impactful solutions!</h3>
+          </Col>
+          <Col md="4" className="footer-copywright">
+            <h3>Copyright © {year}</h3>
+            <img src={logo} className="img-fluid logo" alt="brand" />
+          </Col>
+          <Col md="4" className="footer-body">
+            <ul className="footer-icons">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/19sajib"
+                  style={{ color: "white" }}
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/19sajib"
+                  style={{ color: "white" }}
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/19sajib/"
+                  style={{ color: "white" }}
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/19sajib"
+                  style={{ color: "white" }}
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    );  
+}
+
+export default Footer
---
+++ src/components/Home/About.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import LaptopImg from "../../assets/home-main.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+
+const About = () => {
+    return (
+        <Container fluid className="home-about-section" id="about">
+          <Container>
+            <Row>
+              <Col md={8} className="home-about-description">
+                <h1 style={{ fontSize: "2.6em" }}>
+                  LET ME <span className="yellow"> INTRODUCE </span> MYSELF
+                </h1>
+                <p className="home-about-body">
+                 Hi, my name is <span className="yellow">Md Abu Bakkar Siddiqe Sajib </span>
+                 and I'm from <span className="yellow"> Dhaka, Bangladesh.</span>
+                <br />
+                <br />
+                I recently graduated with a Bachelor's degree in Computer Science and Engineering in 2021.
+                <br />
+                <br />
+                  As a
+                  <b className="yellow"> Full-Stack </b>developer,  
+                  I enjoy tackling new challenges and continuously expanding my skillset.
+                  <br />
+                  <br />I am proficient in
+                    <b className="yellow"> Javascript, </b>
+                    as well as have knowledge in programming languages such as C, Java, Python,
+                  <b className="yellow"> GraphQL, and Solidity.</b>
+                  <br />
+                  <br />
+                  I have a passion for working
+                  with <b className="yellow">Node.js, MongoDB,</b> and
+                  <i>
+                    <b className="yellow">
+                      {" "}
+                      modern Javascript libraries and frameworks
+                    </b>
+                  </i>
+                  &nbsp; like
+                  <i>
+                    <b className="yellow"> React.js</b>
+                  </i>
+                  <br />
+                  <br />
+                  I am also interested in building new
+                  <i>
+                    <b className="yellow"> Web Technologies and Products, </b>
+                    as well as exploring areas related to
+                    <b className="yellow"> Artificial Intelligence.</b>
+                  </i>
+                  <br />
+                  
+                </p>
+              </Col>
+              <Col md={4} className="myAvtar">
+                <Tilt>
+                  <img src={LaptopImg} className="img-fluid" alt="avatar" />
+                </Tilt>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12} className="home-about-social">
+                <h1>FIND ME ON</h1>
+                <p>
+                Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
+                </p>
+                <ul className="home-about-social-links">
+                  <li className="social-icons">
+                    <a
+                      href="https://github.com/19sajib"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <AiFillGithub />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://twitter.com/19sajib"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <AiOutlineTwitter />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://www.linkedin.com/in/19sajib/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <FaLinkedinIn />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://www.instagram.com/19sajib"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour home-social-icons"
+                    >
+                      <AiFillInstagram />
+                    </a>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      );
+    
+}
+
+export default About
