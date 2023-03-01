983eb5f06fc5a486ab905d0a43a729df94dadd88
ngdream <nguewofoss@gmail.com>
2023-03-01 22:25:05 +0100

    update

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,21 +4,20 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
 import Particle from '../components/Particle'
-import pdf from "../assets/sajib.pdf"
+import pdf from "../assets/ngdream.pdf"
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/sajib.pdf`
 
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
-    
+
     setWidth(window.innerWidth);
   }, []);
 
@@ -39,7 +38,7 @@ const Resume = () => {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
