73aad9e29227e5aca65f2d21e711488c0aa9ad1c
ngdream <96875506+ngdream@users.noreply.github.com>
2023-05-26 00:41:31 +0100

    maj website

--- src/components/Contact/Social.jsx
+++ src/components/Contact/Social.jsx
@@ -10,60 +10,60 @@ import './Social.css'
 
 const Social = () => {
   return (
-    <Container style={{padding: '30px'}}>
-    <Row>
-              <Col md={12} className="contact-social">
-                <div className='contact-text'>
-                <h1>FIND ME ON</h1>
-                <p>
-                Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
-                </p>
-                </div>
-                <ul className="contact-social-links">
-                  <li className="contact-icons">
-                    <a
-                      href="https://github.com/19sajib"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color  contact-social-icons"
-                    >
-                      <AiFillGithub />
-                    </a>
-                  </li>
-                  <li className="contact-icons">
-                    <a
-                      href="https://twitter.com/19sajib"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color  contact-social-icons"
-                    >
-                      <AiOutlineTwitter />
-                    </a>
-                  </li>
-                  <li className="contact-icons">
-                    <a
-                      href="https://www.linkedin.com/in/19sajib/"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color  contact-social-icons"
-                    >
-                      <FaLinkedinIn />
-                    </a>
-                  </li>
-                  <li className="contact-icons">
-                    <a
-                      href="https://www.instagram.com/19sajib"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color contact-social-icons"
-                    >
-                      <AiFillInstagram />
-                    </a>
-                  </li>
-                </ul>
-              </Col>
-            </Row>
-          </Container>
+    <Container style={{ padding: '30px' }}>
+      <Row>
+        <Col md={12} className="contact-social">
+          <div className='contact-text'>
+            <h1>FIND ME ON</h1>
+            <p>
+              Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
+            </p>
+          </div>
+          <ul className="contact-social-links">
+            <li className="contact-icons">
+              <a
+                href="https://github.com/ngdream"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-color  contact-social-icons"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="contact-icons">
+              <a
+                href="https://twitter.com/christ19531"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-color  contact-social-icons"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="contact-icons">
+              <a
+                href="https://www.linkedin.com/in/nguewo-christian-36482a224/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BPqF5jjI0QkK1HxM5moOlgQ%3D%3D"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-color  contact-social-icons"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="contact-icons">
+              <a
+                href="https://www.instagram.com/ngdream1975"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-color contact-social-icons"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,65 +9,66 @@ import { FaLinkedinIn } from "react-icons/fa";
 import logo from "../../assets/sLogo.png";
 
 const Footer = () => {
-    let date = new Date();
-    let year = date.getFullYear();
-    return (
-      <Container fluid className="footer">
-        <Row>
-          <Col md="4" className="footer-copywright">
-            <h3>Dedicated to creating impactful solutions!</h3>
-          </Col>
-          <Col md="4" className="footer-copywright">
-            <h3>Copyright © {year}</h3>
-            <img src={logo} className="img-fluid logo" alt="brand" />
-          </Col>
-          <Col md="4" className="footer-body">
-            <ul className="footer-icons">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/19sajib"
-                  style={{ color: "white" }}
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://twitter.com/19sajib"
-                  style={{ color: "white" }}
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/19sajib/"
-                  style={{ color: "white" }}
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/19sajib"
-                  style={{ color: "white" }}
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    );  
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Dedicated to creating impactful solutions!</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year}</h3>
+          <img src={logo} className="img-fluid logo" alt="brand" />
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/ngdream"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/christ19531"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/nguewo-christian-36482a224/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BPqF5jjI0QkK1HxM5moOlgQ%3D%3D"
+
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/ngdream1975/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Footer
